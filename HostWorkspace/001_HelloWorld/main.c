058596a0b974e4e2e5322f6a08fe5bb67bd46329
nemus <nemanjadjekic93@gmail.com>
2020-03-28 15:43:30 +0100

    Updated .gitignore file and added HelloWorld files for host and target projects.

---
+++ HostWorkspace/001_HelloWorld/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 28, 2020
+ *      Author: nemus
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World!\n");
+
+	printf("Press 'Enter' to leave this program!\n");
+
+	getchar();
+
+	return 0;
+}
+
