45127574f51d2cec56ba5c2588e6124028b14fd4
nemus <nemanjadjekic93@gmail.com>
2020-03-29 13:24:28 +0200

    Added ITM debuge configuration

--- TargetWorkspace/001_HelloWorld/Src/main.c
+++ TargetWorkspace/001_HelloWorld/Src/main.c
@@ -21,7 +21,10 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#include <stdio.h>
+
 int main(void)
 {
+	printf("Hello World!\n");
 	for(;;);
 }
