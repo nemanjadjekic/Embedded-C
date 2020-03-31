682ff81def17f2dc84444b29f67239d9788dd0fd
nemus <nemanjadjekic93@gmail.com>
2020-03-31 19:06:32 +0200

    While Loop Intro Exercise added

---
+++ HostWorkspace/013_WhileLoopIntro/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 31, 2020
+ *      Author: nemus
+ */
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i = 1;
+
+	while(i <= 10)
+	{
+		printf("\nNumber value %d", i);
+		i++;
+	}
+
+	return 0;
+}
+
